12b3a80396e3aa303363794f76c3a7bd9e9d66e5
Gary235 <berkman.gary@gmai.com>
2020-12-12 11:01:03 -0300

    splash screen

---
+++ app/src/main/java/com/example/neo_portalrap/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.neo_portalrap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            //Write whatever to want to do after delay specified (1 sec)
+            Intent intent;
+            intent = new Intent(getApplicationContext() ,MainActivity.class);
+            startActivity(intent);
+            finish();
+            Log.d("Handler", "Running Handler");
+        }, 1500);
+
+
+    }
+}
