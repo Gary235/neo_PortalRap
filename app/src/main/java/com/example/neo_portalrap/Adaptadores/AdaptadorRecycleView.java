e038fa07bc46cb44f30c082da34dd9daefdde492
Gary235 <berkman.gary@gmai.com>
2021-01-19 19:53:08 -0300

    .
    Signed-off-by: Gary235 <berkman.gary@gmai.com>

--- app/src/main/java/com/example/neo_portalrap/Adaptadores/AdaptadorRecycleView.java
+++ app/src/main/java/com/example/neo_portalrap/Adaptadores/AdaptadorRecycleView.java
@@ -1,78 +1,87 @@
 package com.example.neo_portalrap.Adaptadores;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.graphics.Bitmap;
+
+import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.ConnectivityManager.NetworkCallback;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.net.NetworkRequest.Builder;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.neo_portalrap.Clases.Base;
+import com.example.neo_portalrap.MainActivity;
 import com.example.neo_portalrap.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.makeramen.roundedimageview.RoundedImageView;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class AdaptadorRecycleView extends RecyclerView.Adapter<AdaptadorRecycleView.MyViewHolder> {
     ArrayList<Base> mDataset;
     final ArrayList<Base> mDatasetcopy = new ArrayList<>();
     Context miContexto;
+    public static boolean isNetworkConnected = false;
+    String tag;
+    String path_file, carpeta = "/neo portal rap/bases/";
+    File localFile;
+    File file;
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    int counterReproduccion = 0;
 
-    public AdaptadorRecycleView(ArrayList<Base> myDataset, Context context) {
+    public AdaptadorRecycleView(ArrayList<Base> myDataset, Context context, String tag) {
         this.mDataset = myDataset;
         this.miContexto = context;
+        this.tag = tag;
 
         mDatasetcopy.addAll(mDataset);
     }
 
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView nombre, artista, duracion;
-        RoundedImageView imagen;
-        ImageButton play, fav;
-
-        public MyViewHolder(View v) {
-            super(v);
-            nombre = v.findViewById(R.id.txt_nombre_base);
-            artista = v.findViewById(R.id.txt_artista_base);
-            duracion = v.findViewById(R.id.txt_duracion_base);
-            play = v.findViewById(R.id.btn_play_base);
-            fav = v.findViewById(R.id.btn_fav_base);
-            imagen = v.findViewById(R.id.img_base);
-
-        }
-    }
-
-    @Override
-    public AdaptadorRecycleView.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                                int viewType) {
-        View v =  LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_recycler, parent, false);
-
-        MyViewHolder vh = new MyViewHolder(v);
-
-        return vh;
-    }
-
-
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) miContexto.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkRequest.Builder builder = new NetworkRequest.Builder();
+
+            connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback(){
+                @Override
+                public void onAvailable(Network network) {
+                    isNetworkConnected = true; // Global Static Variable
+                }
+                @Override
+                public void onLost(Network network) {
+                    isNetworkConnected = false; // Global Static Variable
+                }
+            }
+
+            );
+            isNetworkConnected = false;
+        }
+        catch (Exception e){
+            isNetworkConnected = false;
+        }
 
         holder.artista.setText(mDataset.get(position).getArtista());
         holder.duracion.setText(mDataset.get(position).getDuracion());
@@ -87,7 +96,8 @@ public class AdaptadorRecycleView extends RecyclerView.Adapter<AdaptadorRecycleV
         holder.play.setImageResource(R.drawable.ic_play);
         holder.fav.setImageResource(R.drawable.ic_corazon_vacio);
 
-        StorageReference mStorage = FirebaseStorage.getInstance().getReference();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference mStorage = storage.getReference();
         mStorage.child("info_bases/imagenes/" + mDataset.get(position).getImagen())
                 .getDownloadUrl()
                 .addOnSuccessListener(uri ->
@@ -176,6 +186,27 @@ public class AdaptadorRecycleView extends RecyclerView.Adapter<AdaptadorRecycleV
     }
 
     @Override
+    public AdaptadorRecycleView.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                int viewType) {
+        View v =  LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recycler, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+
+        return vh;
+    }
+
+    public Boolean encontrarBeat(String nombre){
+        String path = Environment.getExternalStorageDirectory().toString() +
+                "/neo portal rap/bases/" +
+                nombre;
+        File f = new File(path);
+
+
+        return f.exists();
+    }
+
+    @Override
     public int getItemCount() {
         return mDataset.size();
     }
@@ -194,6 +225,29 @@ public class AdaptadorRecycleView extends RecyclerView.Adapter<AdaptadorRecycleV
             }
         }
         notifyDataSetChanged();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nombre, artista, duracion;
+        RoundedImageView imagen;
+        ImageButton play, fav;
+        ImageView descargar;
+        CircularProgressIndicator progress_descarga;
+
+
+        public MyViewHolder(View v) {
+            super(v);
+            nombre = v.findViewById(R.id.txt_nombre_base);
+            artista = v.findViewById(R.id.txt_artista_base);
+            duracion = v.findViewById(R.id.txt_duracion_base);
+            play = v.findViewById(R.id.btn_play_base);
+            fav = v.findViewById(R.id.btn_fav_base);
+            imagen = v.findViewById(R.id.img_base);
+            descargar = v.findViewById(R.id.descargar);
+            progress_descarga = v.findViewById(R.id.carga_descarga);
+
+
+        }
     }
 
 
